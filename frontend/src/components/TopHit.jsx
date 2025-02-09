b3a0bf8d3090bfbbb7ce77e3e3f492451151d7c0
Htet Aung Lwin <htetaunglwin.canon@gmail.com>
2025-02-09 08:32:33 +0630

    songItem is working with api

--- frontend/src/components/TopHit.jsx
+++ frontend/src/components/TopHit.jsx
@@ -1,21 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SongItem from "./SongItem";
+import { getTopHits } from "../../songApi";
 
 const TopHit = () => {
+  const [songList, setSongList] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const result = await getTopHits();
+        setSongList(result);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getData();
+  }, []);
+  console.log(songList);
+
   return (
     <div className="row-span-3 bg-primary rounded-xl p-6 px-10">
       <div className="text-3xl font-semibold mb-8 bg-gradient-to-r from-accent via-green-300 to-accent bg-clip-text text-transparent animate-gradient">
         Top Hit Songs
       </div>
-      <div className="flex flex-col gap-4 ">
-        {/* max is 7  */}
-        <SongItem />
-        <SongItem />
-        <SongItem />
-        <SongItem />
-        <SongItem />
-        <SongItem />
-        <SongItem />
+      <div className="flex flex-col gap-4">
+        {/* Limit results to a maximum of 7 */}
+        {songList.slice(0, 7).map((song, index) => (
+          <SongItem key={index} song={song} />
+        ))}
       </div>
     </div>
   );
